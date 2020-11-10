b7099dd8e37e05749ef07685b8ade4017c817dc6
Solvie <lee.solvie@gmail.com>
2020-11-09 23:46:19 -0500

    update name library

--- src/models/human.js
+++ src/models/human.js
@@ -1,10 +1,10 @@
-import { makeAutoObservable } from "mobx"
-const random_name = require('node-random-name');
+import { makeAutoObservable } from 'mobx';
+const Chance = require('chance');
 
 export default class Human {
     constructor(){
         this.birthday = new Date();
-        this.name = random_name();
+        this.name = new Chance().name();
         makeAutoObservable(this);
         console.log(`Hello world my name is ${this.name} and I was born ${this.birthday}`);
     }
